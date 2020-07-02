ed229517a340c8645c8ac35034236d30dd673335
Amritanshu Pandey <amp.msit@gmail.com>
2020-07-02 22:08:04 +1000

    metadata in a post is now a struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![feature(proc_macro_hygiene)]
 mod post;
-mod theme;
 mod util;
 pub use post::Post;
 
 fn main() {
-    let path = "content/posts/post1.md";
-    let post1 = Post::parse(String::from(path));
+    //    let path = "content/posts/post1.md";
+    //    let post1 = Post::parse(String::from(path));
     println!("{:?}", util::find_all_posts());
 }
--- src/post.rs
+++ src/post.rs
@@ -1,15 +1,19 @@
 use crate::util::{md_to_html, read_file};
 extern crate toml;
-use toml::Value;
 
 #[derive(Debug)]
 pub struct Post {
+    metadata: Metadata,
+    content: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct Metadata {
     title: String,
     date: String,
     draft: bool,
     categories: Vec<toml::Value>,
     tags: Vec<toml::Value>,
-    content: String,
 }
 
 impl Post {
@@ -17,35 +21,18 @@ impl Post {
         let inputcontent = read_file(path);
 
         let content_parts: Vec<&str> = inputcontent.split("<post-metadata>").collect();
+        //let metadata_unparsed = format!("{:?}", content_parts[1]);
         let metadata_unparsed = content_parts[1];
         let mdcontent = content_parts[2];
 
-        let metadata_parsed: Value =
-            toml::from_str(metadata_unparsed).expect("Unable to parse toml");
+        println!("{}", metadata_unparsed);
+        let metadata_parsed: Metadata =
+            toml::from_str(metadata_unparsed).expect("Unable to parse toml metadata");
 
         let htmlcontent = md_to_html(mdcontent);
 
         Post {
-            title: match metadata_parsed["title"].as_str() {
-                Some(k) => k.to_string(),
-                _ => String::from(""),
-            },
-            date: match metadata_parsed["date"].as_str() {
-                Some(k) => k.to_string(),
-                _ => String::from(""),
-            },
-            draft: match metadata_parsed["draft"].as_bool() {
-                Some(k) => k,
-                _ => false,
-            },
-            categories: match metadata_parsed["categories"].as_array() {
-                Some(k) => k.clone(),
-                _ => vec![],
-            },
-            tags: match metadata_parsed["tags"].as_array() {
-                Some(k) => k.clone(),
-                _ => vec![],
-            },
+            metadata: metadata_parsed,
             content: htmlcontent,
         }
     }
